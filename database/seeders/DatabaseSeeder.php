54c96b7c1e2d2a486044cb86573c07558e0e45e9
JovannyH12 <jovannyhernandez@micorreo.upp.edu.mx>
2024-07-05 11:44:47 -0600

    Proyecto

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Jovas', 
+            'email' => 'jo@example.com', 
+            'password' => bcrypt('147.852C'), 
+            'email_verified_at' => time()
+            
+        ]);
+
+        Project::factory()
+        ->count(30)
+        ->hasTasks(30)
+        ->create();
+
+    }
+}
